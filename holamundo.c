3630c1880f930b1efb8bc7801de1eea406f0dd3b
iesPadreMan <154273045+iesPadreMan@users.noreply.github.com>
2023-12-18 21:04:58 +0100

    Add files via upload

---
+++ holamundo.c
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CTemplate.c to edit this template
+ */
+#include <stdio.h>
+#include "hola.h"
+
+void hola (const char * nombre)
+{
+  printf ("¡Hola, %s!\n", nombre);
+}
